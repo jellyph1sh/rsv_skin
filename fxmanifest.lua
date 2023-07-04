e62f2579dc0ca0405df02525b847b4aa2adeeb02
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-07-04 17:18:02 +0200

    :rocket: | Deploy resource structure

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "Customize your character's appearance."
+author "JellyPh1sh"
+
+client_scripts {
+    "client/*.lua"
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+shared_scripts {
+    "shared/*.lua"
+}
